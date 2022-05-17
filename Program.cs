4d2b77f0f440a024d2e85ca036e06580146454cd
rsoto1234 <89556340+rsoto1234@users.noreply.github.com>
2022-05-17 19:20:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main()
+    {
+        //string str;
+        int i, wrd, l;
+
+
+        //str = Console.ReadLine();
+        string str = System.IO.File.ReadAllText("Input.txt");
+
+        l = 1;
+        wrd = 1;
+
+        /* loop till end of string */
+        while (l <= str.Length - 1)
+        {
+            /* check whether the current character is white space or new line or tab character*/
+            if ((str[l] == '.' || str[l] == ' ' || str[l] == '\t' || str[l] == '!' || str[l] == ',' || str[l] == '?' || str[l] == '\n') && (str[l - 1] == 't' || str[l - 1] == 'e'))
+            {
+                wrd++;
+            }
+
+
+            l++;
+        }
+
+        Console.Write("There are {0} words ending in t or e", wrd);
+    }
+}
